9b9982117b1efaa8f3248181b177704ac603a39e
Leo <leononame@lho.io>
2017-01-21 13:40:14 +0100

    Infinite repeat

--- Game.py
+++ Game.py
@@ -21,36 +21,38 @@ class Game:
         pygame.key.set_repeat(1, 30)
         # Initialize window
         self.screen = pygame.display.set_mode((self.scr_width, self.scr_height))
-        # Load map
-        self.tiled_map = load_pygame('assets/maps/lvl0.tmx')
         # Window caption
         pygame.display.set_caption("Tilemap game")
         # Mouse invisible
         pygame.mouse.set_visible(0)
         # clock
         self.clock = pygame.time.Clock()
-        # Player position
-        self.player_dx = int(self.scr_width / 32 / 2) - 1
-        self.player_dy = int(self.scr_height / 32 / 2) - 1
-        # Camera position
-        camera_x = 72 - self.player_dx
-        camera_y = 70 - self.player_dy
-        # Create game objects
-        # Create player
-        self.player = Player.Player(self.player_dx, self.player_dy, camera_x, camera_y)
-        # Create map
-        self.current_map = Map.Map(camera_x, camera_y, self.tiled_map)
         # FPS
         self.fps = 30
 
     def run(self):
+        # Player position
+        player_dx = int(self.scr_width / 32 / 2) - 1
+        player_dy = int(self.scr_height / 32 / 2) - 1
+        # Camera position
+        camera_x = 72 - player_dx
+        camera_y = 70 - player_dy
+        # Load map
+        tiled_map = load_pygame('assets/maps/lvl0.tmx')
+
+        # Create game objects
+        # Create player
+        player = Player.Player(player_dx, player_dy, camera_x, camera_y)
+        # Create map
+        current_map = Map.Map(camera_x, camera_y, tiled_map)
+
         # Flow control
         done = False
 
         while not done:
             # Temporarily generate water here
             for i in range(0, 10):
-                self.generateSinkhole(self.tiled_map, random.randint(80, 400), random.randint(80, 400), random.randint(3, 16), random.randint(3, 12))
+                self.generateSinkhole(tiled_map, random.randint(80, 400), random.randint(80, 400), random.randint(3, 16), random.randint(3, 12))
             self.clock.tick(self.fps)
             # Fill screen
             self.screen.fill((198, 209, 255))
@@ -67,14 +69,14 @@ class Game:
                         pygame.event.post(pygame.event.Event(pygame.QUIT))
                     key = event.key
                     # pass keys to map if player is not colliding in order to move camera
-                    if not self.player.is_colliding(key, self.tiled_map):
-                        self.current_map.handle_input(key)
+                    if not player.is_colliding(key, tiled_map):
+                        current_map.handle_input(key)
 
             # Render
-            self.current_map.render(self.screen)
-            self.player.render(self.screen)
+            current_map.render(self.screen)
+            player.render(self.screen)
 
-            if self.player.is_dead(self.tiled_map):
+            if player.is_dead(tiled_map):
                 done = True
                 self.display_game_over()
 
@@ -101,7 +103,7 @@ class Game:
         # Flip display
         pygame.display.flip()
 
-    def wait_exit(self):
+    def wants_repeat(self):
         exit = False
         while not exit:
             self.clock.tick(self.fps)
@@ -116,6 +118,9 @@ class Game:
                     # escape exits game
                     if event.key == pygame.K_ESCAPE:
                         pygame.event.post(pygame.event.Event(pygame.QUIT))
+                    if event.key == pygame.K_SPACE:
+                        return True
+        return False
 
 
     #Only a temporary method
--- waves.py
+++ waves.py
@@ -1,80 +1,11 @@
-import random
-
-import pygame
-from pytmx.util_pygame import load_pygame
-
 import Game
-import Map
-import Player
-import Utils
-
-
-def generateSinkhole(tiled_map, xpos, ypos, width, height):
-    fringe_layer = tiled_map.layers[1]
-    collision_layer = tiled_map.layers[2]
-    asset_layer = tiled_map.layers[3]
-    topl, top, topr = asset_layer.data[0][0], asset_layer.data[0][1], asset_layer.data[0][2]
-    l, w, r = asset_layer.data[1][0], asset_layer.data[1][1], asset_layer.data[1][2]
-    bottoml, bottom, bottomr = asset_layer.data[2][0], asset_layer.data[2][1], asset_layer.data[2][2]
-
-    for i in range(width):
-        for j in range(height):
-            if fringe_layer.data[ypos + j][xpos + i] in [topl, top, topr, l, w, r, bottom, bottomr, bottoml]:
-                fringe_layer.data[ypos + j][xpos + i] = w
-                collision_layer.data[ypos + j][xpos + i] = w
-                continue
-            # Corners
-            if i is 0 and j is 0:  # top left corner
-                pos = topl
-            elif i is width - 1 and j is 0: # top right corner
-                pos = topr
-            elif i is 0 and j is height - 1:  # bot left corner
-                pos = bottoml
-            elif i is width - 1 and j is height - 1: # bot right corner
-                pos = bottomr
-            # Borders
-            elif i is 0: # left border
-                pos = l
-            elif i is width - 1: # right border
-                pos = r
-            elif j is 0: # top border
-                pos = top
-            elif j is height - 1: # bot border
-                pos = bottom
-            else:
-                pos = w
-
-            fringe_layer.data[ypos + j][xpos + i] = pos
-            if pos is w or pos is top:
-                collision_layer.data[ypos + j][xpos + i] = pos
-
-
-# Display a game over message
-def display_game_over(screen):
-    # new surface
-    s = pygame.Surface((screen.get_width(), screen.get_height()))
-    # Set transparent
-    s.set_alpha(128)
-    # Fill black
-    s.fill((0, 0, 0))  # this fills the entire surface
-    screen.blit(s, (0, 0))  # (0,0) are the top-left coordinates
-    # GUI Tile
-    gui_tiles = Utils.load_image("assets/RPG GUI/RPG_GUI_v1.png")
-    screen.blit(gui_tiles, (100, 100), pygame.Rect(15, 115, 300, 80))
-    # Initialize font
-    font = pygame.font.SysFont("monospace", 40)
-    # Render text
-    label = font.render("Game Over", 1, (204, 153, 0))
-    screen.blit(label, (135, 115))
-    # Flip display
-    pygame.display.flip()
-    return True
 
 
 def main():
     game = Game.Game()
     game.run()
-    game.wait_exit()
+    while(game.wants_repeat()):
+        game.run()
 
 
 if __name__ == '__main__':
