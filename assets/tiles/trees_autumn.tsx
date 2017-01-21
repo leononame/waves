aadb40909e188b5d5b33851a333111a341e06b7b
Leo <leononame@lho.io>
2017-01-21 16:03:54 +0100

    Trees and some weird blocking

---
+++ assets/tiles/trees_autumn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="trees_autumn" tilewidth="32" tileheight="32" tilecount="28" columns="7">
+ <image source="../trees/autumn_4.png" width="224" height="128"/>
+</tileset>
---
+++ assets/tiles/trees_green.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="trees_green" tilewidth="32" tileheight="32" tilecount="28" columns="7">
+ <image source="../trees/green trees_1.png" width="224" height="128"/>
+</tileset>
