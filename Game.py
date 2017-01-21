eaa190061b26510e7f7d06669d7ebd43d78d605f
Leo <leononame@lho.io>
2017-01-21 13:18:14 +0100

    Move game to own class

--- Game.py
+++ Game.py
@@ -3,6 +3,7 @@ from pytmx.util_pygame import load_pygame
 
 import Map
 import Player
+import Utils
 
 
 class Game:
@@ -17,7 +18,7 @@ class Game:
         # Enable repitition of event when key is being pressed continuously
         pygame.key.set_repeat(1, 30)
         # Initialize window
-        self.screen = pygame.display.set_mode(self.scr_width, self.scr_height)
+        self.screen = pygame.display.set_mode((self.scr_width, self.scr_height))
         # Load map
         self.tiled_map = load_pygame('assets/maps/lvl0.tmx')
         # Window caption
@@ -29,10 +30,69 @@ class Game:
         # Player position
         self.player_dx = int(self.scr_width / 32 / 2) - 1
         self.player_dy = int(self.scr_height / 32 / 2) - 1
+        # Camera position
+        camera_x = 72 - self.player_dx
+        camera_y = 70 - self.player_dy
         # Create game objects
         # Create player
-        self.player = Player.Player(self.player_dx, self.player_dy, self.camera_x, self.camera_y)
+        self.player = Player.Player(self.player_dx, self.player_dy, camera_x, camera_y)
         # Create map
-        self.current_map = Map.Map(self.camera_x, self.camera_y, self.tiled_map)
+        self.current_map = Map.Map(camera_x, camera_y, self.tiled_map)
         # FPS
         self.fps = 30
+
+    def run(self):
+        # Flow control
+        done = False
+
+        while not done:
+            # Tick
+            self.clock.tick(self.fps)
+            # Fill screen
+            self.screen.fill((198, 209, 255))
+            # Check events
+            # Get all events
+            for event in pygame.event.get():
+                # exit game
+                if event.type == pygame.QUIT:
+                    done = True
+                # controls
+                if event.type == pygame.KEYDOWN:
+                    # escape exits game
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.event.post(pygame.event.Event(pygame.QUIT))
+                    key = event.key
+                    # pass keys to map if player is not colliding in order to move camera
+                    if not self.player.is_colliding(key, self.tiled_map):
+                        self.current_map.handle_input(key)
+
+            # Render
+            self.current_map.render(self.screen)
+            self.player.render(self.screen)
+
+            if self.player.is_dead(self.tiled_map):
+                done = True
+                self.display_game_over()
+
+            # flip screen
+            pygame.display.flip()
+
+    # Display a game over message
+    def display_game_over(self):
+        # new surface
+        s = pygame.Surface((self.screen.get_width(), self.screen.get_height()))
+        # Set transparent
+        s.set_alpha(128)
+        # Fill black
+        s.fill((0, 0, 0))  # this fills the entire surface
+        self.screen.blit(s, (0, 0))  # (0,0) are the top-left coordinates
+        # GUI Tile
+        gui_tiles = Utils.load_image("assets/RPG GUI/RPG_GUI_v1.png")
+        self.screen.blit(gui_tiles, (100, 100), pygame.Rect(15, 115, 300, 80))
+        # Initialize font
+        font = pygame.font.SysFont("monospace", 40)
+        # Render text
+        label = font.render("Game Over", 1, (204, 153, 0))
+        self.screen.blit(label, (135, 115))
+        # Flip display
+        pygame.display.flip()
--- waves.py
+++ waves.py
@@ -3,6 +3,7 @@ import random
 import pygame
 from pytmx.util_pygame import load_pygame
 
+import Game
 import Map
 import Player
 import Utils
@@ -71,78 +72,11 @@ def display_game_over(screen):
 
 
 def main():
-    pygame.init()
-    tile_size = 32
-    scr_width, scr_height = 30 * tile_size, 30 * tile_size
-    pygame.key.set_repeat(1, 30)
-    screen = pygame.display.set_mode((scr_width, scr_height))
-    tiled_map = load_pygame('assets/maps/lvl0.tmx')
-
-    pygame.display.set_caption("Tilemap game")
-    pygame.mouse.set_visible(0)
-
-    # clock
-    clock = pygame.time.Clock()
-
-    # flow control
-    done = False
-
-    player_dx = int(scr_width / 32 / 2) - 1
-    player_dy = int(scr_height / 32 / 2) - 1
-    camera_x, camera_y = 72 - player_dx, 70 - player_dy
-
-    # Create game objects
-    # Create player
-    player = Player.Player(player_dx, player_dy, camera_x, camera_y)
-    # Create map
-    current_map = Map.Map(camera_x, camera_y, tiled_map)
-    # FPS
-    fps = 30
-
-    generateSinkhole(tiled_map, 80, 80, 5, 7)
-
-    count = 0
-    # Start loop
-    while not done:
-        count += 1
-        if count is fps / 2:
-            count = 0
-        generateSinkhole(tiled_map, random.randint(70, 300), random.randint(70, 300), random.randint(3, 12), random.randint(3, 8))
-        clock.tick(fps)
-        # fill screen
-        screen.fill((198, 209, 255))
-        # Check events
-
-        # Get all events
-        for event in pygame.event.get():
-            # exit game
-            if event.type == pygame.QUIT:
-                done = True
-
-            # controls
-            if event.type == pygame.KEYDOWN:
-                # escape exits game
-                if event.key == pygame.K_ESCAPE:
-                    pygame.event.post(pygame.event.Event(pygame.QUIT))
-                key = event.key
-                if not player.is_colliding(key, tiled_map):
-                    current_map.handle_input(key)
-
-        current_map.render(screen)
-        player.render(screen)
-
-        if player.is_dead(tiled_map):
-            done = True
-            display_game_over(screen)
-
-        done = True
-        display_game_over(screen)
-        # flip screen
-        pygame.display.flip()
-
+    game = Game.Game()
+    game.run()
     exit = False
     while not exit:
-        clock.tick(fps)
+        game.clock.tick(game.fps)
         # Get all events
         for event in pygame.event.get():
             # exit game
